266ac6244319265eff6f1accba98e3c46b259f0a
jessicaamery <1601587@uad.ac.uk>
2019-04-16 09:56:16 +0100

    removing unused queries

--- app/src/main/java/whalethedvaa/whalethedvaa/DaoAccess.java
+++ app/src/main/java/whalethedvaa/whalethedvaa/DaoAccess.java
@@ -7,32 +7,13 @@ import android.arch.persistence.room.*;
 public @Dao
 interface DaoAccess {
 
-   // @Insert
-    //void insertOnlySingleUser(int uid, String emailAddress, String password);
-
     @Insert
     void insertUserTest(Emails emails);
 
     @Query("SELECT * FROM Emails WHERE uid=:uid")
     Emails fetchOneEmailsByEmailsID(int uid);
 
-    @Update
-    void updateEmails(Emails emails);
-
-    @Delete
-    void deleteEmail(Emails emails);
-
     @Query("SELECT COUNT(emailAddress) FROM Emails")
     int getRowCount();
-
-    //@Query("SELECT COUNT(*) FROM Emails WHERE emailAddress=:searchText")
-    //int emailExists(String searchText);
-
-    //@Query(searchText)
-    //int emailExists(String searchText);
-
-
-    //@Query("SELECT * FROM emails")
-    //Emails fetchemails(Emails emails);
 }
 
